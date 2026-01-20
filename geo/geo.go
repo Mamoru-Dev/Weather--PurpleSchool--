bf3cfe3d08f4287ac7f34334c4bb8ceb1d8a939f
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2026-01-20 21:13:44 +0300

    developed CheckCity function

--- geo/geo.go
+++ geo/geo.go
@@ -1,6 +1,7 @@
 package geo
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"io"
@@ -11,12 +12,20 @@ type GeoData struct {
 	City string `json:"city"`
 }
 
+type CityPopulationResponse struct {
+	Error bool `json:"error"`
+}
+
 func GetMyLocation(city string) (*GeoData, error) {
 	// Если город передан => Используем его
 	if city != "" {
-		return &GeoData{
-			City: city,
-		}, nil
+		if CheckCity(city) {
+			return &GeoData{
+				City: city,
+			}, nil
+		} else {
+			panic("Такого города нет")
+		}
 	}
 
 	// Если город не передан => Ищем его по IP
@@ -24,6 +33,7 @@ func GetMyLocation(city string) (*GeoData, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		return nil, errors.New("NOT200")
 	}
@@ -34,4 +44,24 @@ func GetMyLocation(city string) (*GeoData, error) {
 	var geo GeoData
 	json.Unmarshal(body, &geo)
 	return &geo, nil
+}
+
+func CheckCity(city string) bool {
+	// Создаем объект body для Post-запроса
+	postBody, _ := json.Marshal(map[string]string{"city": city})
+
+	// Post(url, кодировка, body)
+	apiUrl := "https://countriesnow.space/api/v0.1/countries/population/cities"
+	resp, err := http.Post(apiUrl, "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	var populationResponse CityPopulationResponse
+	json.Unmarshal(body, &populationResponse)
+	return !populationResponse.Error
 }
